55b5567c184366fad868df58019172eaaafae74a
Manas Mahale <manas.mahale2002@gmail.com>
2024-02-25 04:51:44 +0530

    update to run.sh

--- run.sh
+++ run.sh
@@ -1,20 +1,30 @@
 #!/bin/bash
 
-mkdir data/raw data/final data/processed
+# Function to get directory of a file
+get_directory() {
+    echo "$(cd "$(dirname "$1")" && pwd)"
+}
 
+# Create directories
+mkdir -p data data/raw data/final data/processed
+
+# Download dataset
 wget -c https://media.githubusercontent.com/media/molecularsets/moses/master/data/dataset_v1.csv
 mv dataset_v1.csv ./data/raw/moses_dataset.csv
 
 # List of Python file names in the desired order
-python_files=("./src/fragment_tokenizer/moses_train_test_split.py", 
-              "./src/fragment_tokenizer/encode_moses.py",
-              "./src/model/tokenizer.py",
-              "./src/model/model.py"
+python_files=("src/fragment_tokenizer/moses_train_test_split.py" 
+              "src/fragment_tokenizer/encode_moses.py"
+              "src/model/tokenizer.py"
+              "src/model/model.py"
 )
 
 # Iterate over each Python file and run it
 for file in "${python_files[@]}"; do
     echo "Running $file..."
-    python "$file"
+    dir=$(get_directory "$file")
+    cd "$dir" || exit
+    python "$(basename "$file")"
+    cd - || exit
     echo "Finished running $file."
 done
